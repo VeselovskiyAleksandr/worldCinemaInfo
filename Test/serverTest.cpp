2b21827bddd0e40960cb4385c3dd99e6c214e3a5
VeselovskiyAleksandr <VeselovskiyAleksandr@mail.ru>
2025-06-04 15:17:51 +0300

    error Test

--- Test/serverTest.cpp
+++ Test/serverTest.cpp
@@ -1,5 +1,10 @@
 #include "gtest/gtest.h"
 #include "ConverterJSON.h"
+#include "Entry.h"
+#include "Configuration.h"
+#include <map>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -8,54 +13,85 @@ TEST(sampleCTestCase, sampleTest)
 	EXPECT_EQ(1, 1);
 }
 
+TEST(ConverterJSONTest, getRequestsFunctionTest)
+{
+	ConverterJSON converterJSON;
+	map <int, vector< string>>getRequestsTest;
+	converterJSON.getRequestsFunction(getRequestsTest);
+	for (auto i : getRequestsTest)
+	{
+		if (i.first == 2)
+		for(auto j: i.second)
+		{
+			EXPECT_TRUE(j== "tank this other");
+		}
+	}
+}
 
-	//TEST(ConverterJSON, sampleMultimap)
-	//{
-	//	ConverterJSON converterJSON;
-	//	vector< string > requestsTest = { "Etush", "Vysotskiy", "Kuravlyov", "my" };
-	//	pair<int, vector< string>> oPair(0, requestsTest);
-	//	map <int, vector< string>>getRequests;
-		//	getRequests.insert(oPair);
-			//multimap<string, vector< Entry>> countWordsMap;
+	TEST(ConverterJSONTest, wordCountFunctionTest)
+	{
+		ConverterJSON converterJSON;
+		Entry entry;
+		vector< Entry> vectorEntry;
+	//	vector< string > requestsTest = { "Etush", "Vysotskiy", "Kuravlyov", "my"};
+//		pair<int, vector< string>> oPair(0, requestsTest);
+//		map <int, vector< string>>getRequestsTest;
+	//	EXPECT_EQ(getRequestsTest.size(), 0);
+	//		getRequests.insert(oPair);
+
+
+		multimap<string, vector< Entry>> countWordsMap;
+
+
+//		converterJSON.getRequestsFunction(getRequestsTest);
+//		ASSERT_EQ(getRequestsTest.size(), 0);
+//		EXPECT_FALSE(getRequestsTest.size()== 5);
+//		EXPECT_EQ(getRequestsTest.size(), 0);
+
+
+			converterJSON.wordCountFunction(countWordsMap);
+
 
 //		TEST(wordCountFunction, sampleMultimap)
-	//	{
-			//		EXPECT_EQ(converterJSON.countWordsMap.size(), 3);
+//		{
+
+	//				EXPECT_EQ(countWordsMapTest.size(), 940);
 	//	}
-		//	for (const auto& pair : converterJSON.countWordsMap)
-		//	{
-			//	if (pair.first == "Etush")
-		//		{
-		//			for (const auto& p : pair.first)
-		//			{
-		//				if (p == entry.docId)
-		//				EXPECT_EQ(entry.docId, 0);
-		//				else if(p== entry.freqWordsCount)
-		//					EXPECT_EQ(entry.freqWordsCount, 1);
-		//			}
-		//		}
-		//		else if (pair.first == "Vysotskiy")
-		//		{
-		//			for (const auto& p : pair.first)
-		//			{
-		//				if (p == entry.docId)
-		//					EXPECT_EQ(entry.docId, 2);
-		//				else if (p == entry.freqWordsCount)
-		//					EXPECT_EQ(entry.freqWordsCount, 2);
-		//			}
-		//		}
-		//		else if (pair.first == "Kuravlyov")
-		//		{?????????????????????????????????
-		//			{
-		//				if (p == entry.docId)
-		//					EXPECT_TRUE(entry.docId, 0|| entry.docId, 1);
-		//				else if (p == entry.freqWordsCount)
-		//					EXPECT_EQ(entry.freqWordsCount, 2);
-		//			}
-		//		}
-		//	}
-		//	delete[] entry;
-		//}
+
+
+//			for (const auto& pair : countWordsMap)
+//			{
+//				if (pair.first == "Etush")
+//				{
+//					for (const auto& p : pair.first)
+//					{
+	//					if (p == entry.docId)
+	//					EXPECT_TRUE(entry.docId== 0);
+	//					else if(p== entry.freqWordsCount)
+	//						EXPECT_TRUE(entry.freqWordsCount== 1);
+//					}
+//				}
+//				else if (pair.first == "Vysotskiy")
+//				{
+//					for (const auto& p : pair.first)
+//					{
+//						if (p == entry.docId)
+//							EXPECT_TRUE(entry.docId== 2);
+//						else if (p == entry.freqWordsCount)
+//							EXPECT_TRUE(entry.freqWordsCount== 2);
+//					}
+//				}
+//				else if (pair.first == "Kuravlyov")
+//					for (const auto& p : pair.first)
+//					{
+//						if (p == entry.docId)
+//							EXPECT_TRUE(entry.docId== 0|| entry.docId== 1);
+//						else if (p == entry.freqWordsCount)
+//							EXPECT_TRUE(entry.freqWordsCount== 2);
+//					}
+	//			}
+	//				delete entry;
+		}
 int main()
 {
 	testing::InitGoogleTest();
